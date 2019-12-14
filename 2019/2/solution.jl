640e0cf5f11a2b10a1b4acfbb2bd0c0006a30039
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-12-14 00:30:13 -0500

    finish day 2

--- 2019/2/solution.jl
+++ 2019/2/solution.jl
@@ -7,15 +7,28 @@ instructions = read("$(@__DIR__)/input.txt", String) |> x-> split(x, ',') .|> x-
 instructions[2] = 12
 instructions[3] = 2
 
-c = Intcode.Computer(instructions)
+# part 1
 
-# iterate(c)
-#
-# c
-# iterate(c, 4)
+c = Intcode.Computer(instructions)
 
 for op in c
     Intcode.execute!(c, op)
 end
 
-c.instructions[0]
+@show c.instructions[0] # answer to part 1
+
+# part 2
+
+for noun in 0:99, verb in 0:99
+    localins = copy(instructions)
+    localins[2] = noun
+    localins[3] = verb
+    Intcode.reset!(c, localins)
+    for op in c
+        Intcode.execute!(c, op)
+    end
+    if c[0] == 19690720
+        @show noun verb 100 * noun + verb
+        break
+    end
+end
